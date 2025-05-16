4cee9f779cd10cae3a3554ea79a067eb3621616b
Kim_Kyoung_Whoe <kw72622@naver.com>
2025-05-16 15:41:19 +0900

    브랜치 초기화설명

--- src/main/java/kr/co/himedia/controller/MainController.java
+++ src/main/java/kr/co/himedia/controller/MainController.java
@@ -15,6 +15,7 @@ public class MainController {
 	public String main() {
 		logger.info("hello! KIM KYOUNG WHOE");
 		logger.info("아무거나 추가!!!!");
+		logger.info("브랜치 초기화 : 특정한 커밋의 상태로 완전히 되돌려 버리는 것");
 		return "{\"msg\":\"OK\"}";
 	}
 	

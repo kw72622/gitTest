de3818c052a9a7d49211970caa1b857c7131353e
Kim_Kyoung_Whoe <kw72622@naver.com>
2025-05-16 15:10:16 +0900

    로거 추가

--- src/main/java/kr/co/himedia/controller/MainController.java
+++ src/main/java/kr/co/himedia/controller/MainController.java
@@ -1,13 +1,19 @@
 package kr.co.himedia.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
+	Logger logger = LoggerFactory.getLogger(getClass());
+	
+	
 	@GetMapping(value = "/")
 	public String main() {
+		logger.info("hello");
 		return "{\"msg\":\"OK\"}";
 	}
 	
